a42cd83202ed8db5aca0f2983f8755372a89b030
Muhammad Junaid <86664791+MuhammadJunaid01@users.noreply.github.com>
2023-03-12 02:18:14 +0600

    leetcode problems

---
+++ dest/problems.js
@@ -0,0 +1,5 @@
+"use strict";
+const merge = (nums1, m, nums2) => {
+    return [0];
+};
+console.log(merge());
---
+++ src/leetcode/problems/problems.ts
@@ -0,0 +1,8 @@
+const merge = (
+  nums1: Array<number>,
+  m: number,
+  nums2: Array<number>
+): Array<number> => {
+  return [0];
+};
+console.log(merge());
